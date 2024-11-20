1afc5ce43b1b6ffea6151bb89339b0f048f674c0
Daniel-derf <danielpardinho321@gmail.com>
2024-11-20 20:23:18 -0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,61 @@
 import { useReducer, useState } from 'react'
 import './App.css'
 
-const reducer = (state, action) => {
+type Task = {
+  id: number,
+  name: string
+}
 
-  switch (action.type){
+type ActionType = 
+  | { type: 'add', payload: { name: string } }
+  | { type: 'delete', payload: { id: number } }
 
+type StateType = Task[]
+
+const reducer = (state: StateType, action: ActionType): StateType => {
+  switch (action.type) {
     case 'add':
-      const newTask = {
-        id:  Math.random(),
+      const newTask: Task = {
+        id: Math.random(),
         name: action.payload.name
       }
-      return [...state, newTask ]
+      return [...state, newTask]
 
     case 'delete':
-      const newTasks = state.filter(
-        task => task.id != action.payload.id
-      )
-      return [...newTasks]
+      return state.filter(task => task.id !== action.payload.id)
+
+    default:
+      return state
   }
-
-
 }
 
 function App() {
   const [tasks, dispatch] = useReducer(reducer, [])
-
   const [taskInput, setTaskInput] = useState('')
 
   const handleAddTask = (taskName: string) => {
-    dispatch({type: 'add', payload: {
-      name: taskName
-    }})
+    dispatch({ type: 'add', payload: { name: taskName } })
   }
 
   const handleDeleteTask = (taskId: number) => {
-    dispatch({type: 'delete', payload: {
-      id: taskId
-    }})
+    dispatch({ type: 'delete', payload: { id: taskId } })
   }
-
 
   return (
     <>
-    <span>TASK: </span>
-    <input type="text" onChange={e =>{
-      setTaskInput(e.target.value)
-    }} />
-    <button onClick={() => handleAddTask(taskInput) } >ADD</button>
+      <span>TASK: </span>
+      <input 
+        type="text" 
+        onChange={e => setTaskInput(e.target.value)} 
+      />
+      <button onClick={() => handleAddTask(taskInput)}>ADD</button>
 
-    {tasks?.map(
-      task => {
-        return <p key={task.id}> {task.name} <button
-        onClick={()=>handleDeleteTask(task.id)} >DELETE</button> </p>
-      }
-    )}
-
+      {tasks.map(task => (
+        <p key={task.id}>
+          {task.name} 
+          <button onClick={() => handleDeleteTask(task.id)}>DELETE</button>
+        </p>
+      ))}
     </>
   )
 }
